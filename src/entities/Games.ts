f42a33b94f537f9424d335d31c0c2d6e07673b83
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2024-01-06 16:17:03 +0530

    Bulid game Attributes

--- src/entities/Games.ts
+++ src/entities/Games.ts
@@ -1,10 +1,14 @@
 import { Platform } from "../hooks/useGames";
+import { Genre } from "./Genre";
+import { Publishers } from "./Publishers";
 
 
 export interface Games {
     id: number;
     slug: string;
     name: string;
+    genres: Genre[];
+    Publishers: Publishers[];
     description_raw: string;
     background_image: string;
     parent_platforms: { platform: Platform; }[];
