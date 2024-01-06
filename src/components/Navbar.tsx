1dbe5575c0a482c1fe349c7bc5bd95fd386c6a73
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2024-01-06 17:05:49 +0530

    fixed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,14 @@ import logo from "../assets/logo.webp";
 import ColorModeswitch from "./ColorModeswitch";
 import SearchInput from "./SearchInput";
 import UseGameQueryStore from "../store/useGamestore";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <HStack padding={"10px"}>
-      <Image src={logo} boxSize={"60px"}></Image>
+      <Link to={"/"}>
+        <Image src={logo} boxSize={"60px"} objectFit={"cover"}></Image>
+      </Link>
       <SearchInput />
       <ColorModeswitch />
     </HStack>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTailer from "../components/GameTailer";
@@ -13,13 +13,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
